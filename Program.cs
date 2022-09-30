717457c35658170d500f4f2be93fe56819350177
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-09-30 10:59:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_3._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to n/a company.\n\nWe are searching for an employe who fits what we're looking for." +
+                "\nSo pls answer these following quations.\n\nHave you graduated high school? pls answer in y or n.");
+            string awe = Console.ReadLine();
+            Console.WriteLine("How old are you");
+            float age = float.Parse(Console.ReadLine());
+
+            if (age <= 22 && awe.ToLower() == "y")
+            {
+                Console.WriteLine("We would like to hire you.");
+            }
+
+            else
+            {
+                Console.WriteLine("Sry but we're searching for someone else.");
+            }
+        }
+    }
+}
