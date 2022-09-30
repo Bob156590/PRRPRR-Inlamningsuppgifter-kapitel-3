4ee0d710c8c72ecc9837a10418a02fd50254930a
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-09-30 10:16:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_3._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how old are you? pls write in only nubers.");
+            float age = float.Parse(Console.ReadLine());
+
+            if (age >= 16 && age <= 19)
+            {
+                Console.WriteLine("You may paticipat in the competition");
+            }
+            if (age <= 16)
+            {
+                Console.WriteLine("You are to young to enter this competition");
+            }
+            else
+            {
+                Console.WriteLine("you are to old to enter this competition");
+            }
+        }
+    }
+}
