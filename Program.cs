33d038285f3fa1b2268964ffb4af19b88271e5b1
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-10-03 16:51:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to our rental company, how long will you be renting our car for?\nPls remeber that the maximum you can pay is SEK 950.");
+            float time = float.Parse(Console.ReadLine());
+            float cost = (time * 80);
+
+            if (cost <= 950)
+            {
+                Console.WriteLine($"That come to {cost}kr.");
+            }
+
+            else
+            {
+                Console.WriteLine("apologise but that would cost more then 950kr");
+            }
+            Console.ReadKey();
+        }
+    }
+}
