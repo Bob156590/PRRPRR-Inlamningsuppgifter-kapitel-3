470814651f3da6a0928d6307fd86d8cd3742a0c4
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-10-05 12:00:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace upgift_3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to C# simple calcualter. Pls write two numbers one after the other. Remember to press enter after each number.");
+            float n1 = float.Parse(Console.ReadLine());
+            float n2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Now choose one of the options bellow.\n1.Addition\n2.Subtraktion\n3.Multiplikation\n4.Division");
+            int l = int.Parse(Console.ReadLine());
+
+
+            switch (l)
+            {
+                case 1:
+                    Console.WriteLine($"\n That comes to {n1 + n2}");
+                    break;
+                case 2:
+                    Console.WriteLine($"\n That comes to {n1 - n2}");
+                    break;
+                case 3:
+                    Console.WriteLine($"\n That comes to {n1 * n2}");
+                    break;
+                case 4:
+                    Console.WriteLine($"\n That comes to {n1 / n2}");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
